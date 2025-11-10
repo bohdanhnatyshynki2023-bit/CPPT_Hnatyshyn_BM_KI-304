889098c4497d9130d11d2fa7854defc2ce49bf50
Bohdan <bohdan.hnatyshyn.ki.2023@lpnu.ua>
2025-11-10 22:13:05 +0200

    lab4

---
+++ Lab4/src/KI304/Hnatyshyn/Lab4/CalcException.java
@@ -0,0 +1,14 @@
+package KI304.Hnatyshyn.Lab4;
+
+public class CalcException extends ArithmeticException {
+
+    public CalcException() {
+        super();
+    }
+
+    public CalcException(String message) {
+        super(message);
+    }
+
+}
+
